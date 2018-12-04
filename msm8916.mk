10d36c1c3292a636f5b3f222e9f58fd7f179a389
dev-harsh1998 <harshitjain6751@gmail.com>
2018-12-04 21:36:25 +0530

    msm8916-common: Switch to kernel thermal management
    * We manage it from kernel now

--- msm8916.mk
+++ msm8916.mk
@@ -303,11 +303,6 @@ PRODUCT_BOOT_JARS += \
 PRODUCT_PACKAGES += \
     textclassifier.smartselection.bundle1
 
-# Thermal
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/thermal/thermal-engine-8916.conf:system/etc/thermal-engine-8916.conf \
-    $(LOCAL_PATH)/thermal/thermal-engine-8939.conf:system/etc/thermal-engine-8939.conf
-
 # USB HAL
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service \
